fbf003f91657ef09b78f96e1b701868f19573d74
vasuvenk <vasu18@gmail.com>
2016-09-28 23:15:53 -0700

    sanitized code and reduced bg image size

--- Hw2/analytics/index.php
+++ Hw2/analytics/index.php
@@ -58,59 +58,59 @@ function landing()
 
 function codes()
 {
-	
-	if(!isset($_REQUEST["arg"]) || empty($_REQUEST["arg"]))
-	{
-		
-		landing();
-		return;
-	}	
-	
 	$placeholdertext="Enter a URL to track";
 	$XXXX="";
 	$YYYY="";
 	$valueoftext="";
 	$lookups=[];
 	$empty_form=true;
-
-	if(isset($_REQUEST["arg2"]) && !empty($_REQUEST["arg2"]))
+	
+	if(isset($_REQUEST["arg"]) && !empty($_REQUEST["arg"]))
 	{
-		$empty_form=false;
-		$valueoftext=$_REQUEST["arg2"];
-		$XXXX=sha1($_REQUEST["arg"].$_REQUEST["arg2"]);
-		$YYYY=sha1($_REQUEST["arg"]);
 
-		if(file_exists("./url_lookups.txt"))
+		if(isset($_REQUEST["arg2"]) && !empty($_REQUEST["arg2"]))
 		{
-			$contents=file_get_contents("./url_lookups.txt");
-			$lookups=unserialize($contents);
-		}
+			$empty_form=false;
+			$valueoftext =$_REQUEST["arg2"];
+			$XXXX=sha1($_REQUEST["arg"].$_REQUEST["arg2"]);
+			$YYYY=sha1($_REQUEST["arg"]);
+
+			if(file_exists("./url_lookups.txt"))
+			{
+				$contents=file_get_contents("./url_lookups.txt");
+				$lookups=unserialize($contents);
+			}
 		
-		$lookups[$XXXX]=$_REQUEST["arg2"];
+			$lookups[$XXXX]=$_REQUEST["arg2"];
 		
 
-		$lookupstofile=serialize($lookups);
-		file_put_contents("./url_lookups.txt",$lookupstofile);	
+			$lookupstofile=serialize($lookups);
+			file_put_contents("./url_lookups.txt",$lookupstofile);	
+		}
+	}
+
+	else
+	{
+		landing();
+		return;
 	}
 ?>
 	<h1>Tracker Codes - Web Page Tagging Analytics</h1>
 	<div class="forminput_info">	
 	<form name="formtrack" method="GET">
 		<input type="hidden" name="activity" value="codes"/>
-<?php
 	
-	print("<input type=\"hidden\" name=\"arg\" value=\"".$_REQUEST["arg"]."\"/>");
-	print("<input type=\"text\" name=\"arg2\" placeholder=\"$placeholdertext\" value=\"$valueoftext\"> </br>");
-?>
+		<input type="hidden" name="arg" value="<?php echo $_REQUEST["arg"];?>">
+		<input type="text" name="arg2" placeholder="<?php echo $placeholdertext;?>" value="<?php echo $valueoftext;?>"> 
 		<input type="submit" name="submitarg2" value="Go" />
 	</form>
 	</div>
 	
 <?php
-	if(!$empty_form)
+	if(!$empty_form && empty($errortext))
 	{	
-		print("<h2>Add the following code to the web page of the site with the url just entered</h2>");
 		$codesnippet="<script src=\"".URL_TO_TRACKER_SITE."/?activity=counts&arg=$YYYY&arg2=$XXXX\"/>";
+		print("<h2>Add the following code to the web page of the site with the url just entered</h2>");
 		print("<span>".htmlentities($codesnippet)."</span>");
 	}
 ?>	
@@ -142,11 +142,15 @@ function counts()
 		}
 		
 		file_put_contents("./counts.txt",serialize($counts));
+		
+		$tracking_result="<script type=\"text/javascript\">var tracking=\"done\";</script>";
+		htmlentities($tracking_result);
 	}
 }
 
 function analytics()
 {
+	
 	print("<h1>View Analytics - Web Page Tagging Analytics</h1>");
 	print("<h2>Analytics for ". $_REQUEST["arg"]."</h2>");
 	$lookups=[];
