e8f5a16e8316cde862378c0c8cbbc7f60a4ac4e2
vasuvenk <vasu18@gmail.com>
2016-10-24 23:13:37 -0700

    changed iindex

--- Hw3/index.php
+++ Hw3/index.php
@@ -1,5 +1,8 @@
 <?php
+namespace cool_name_for_your_group\hw3;
+use cool_name_for_your_group\hw3\controllers as C;
 
+session_start();
 spl_autoload_register(function ($class) {
     // project-specific namespace prefix
     $prefix = 'cool_name_for_your_group\\hw3';
@@ -13,34 +16,57 @@ spl_autoload_register(function ($class) {
         '.php';
     $file = 'src' . $unixify_class_name;
     if (file_exists($file)) {
-        require $file;
+        require_once $file;
     }
 });
 
-use cool_name_for_your_group\hw3\controllers\LandingController;
-use cool_name_for_your_group\hw3\controllers\WriteController;
-
-//require_once('LandingController.php');
-//require_once('WriteController.php');
 if(!isset($_REQUEST['c']) && !isset($_REQUEST['m']))
 {
-    $controller=new LandingController();
-    $controller->invoke();
+	$controller=new C\LandingController();
+	$controller->invoke();
 }
 
 else
 {
-    $controllertocall=$_REQUEST['c'];
-    switch ($controllertocall) {
-        case 'WriteController' :
-            $controller=new WriteController();
-            break;
-        default:
-            $controller= new LandingController();
-    }
-    $methodtoinvoke=$_REQUEST['m'];
-    $controller->$methodtoinvoke();
-    
+	$controllertocall=$_REQUEST['c'];
+	$methodtoinvoke=$_REQUEST['m'];
+	$querystring=$_SERVER['QUERY_STRING'];
+	parse_str($querystring,$request_array);
+	$argumentlist=[];
+	$availablecontrollers=['LandingController','WriteController','ReadStoryController'];
+	
+	if(count($request_array)>2)
+	{
+		$numberofparams=count($request_array);
+		for($i=1;$i<=($numberofparams-2);$i++)
+		{
+			$arname="arg".$i;
+			if(array_key_exists($arname,$request_array))
+			{
+				$argumentlist[]=$request_array[$arname];
+			}
+		}
+		
+		//$controller->$methodtoinvoke($argumentlist);
+	}
+	if(in_array($controllertocall,$availablecontrollers))
+	{
+		//It is a valid controller
+		$controllerclass="C\\".$controllertocall;
+		$controller=new $controllerclass();
+		if(method_exists($controller,$methodtoinvoke))
+		{
+			//it is a valid method in the controller
+			if(empty($argumentlist))
+			{
+				$controller->$methodtoinvoke();
+			}
+			else
+			{
+				$controller->$methodtoinvoke($argumentlist);
+			}
+		}
+	}
+	
 }
 ?>
-
