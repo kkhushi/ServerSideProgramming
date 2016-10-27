cb184a4b326d0be0bc94a96fdffdda3728de19d6
vasuvenk <vasu18@gmail.com>
2016-10-27 00:03:18 -0700

    fix for sanitized input

---
+++ ReadStoryController.php
@@ -0,0 +1,47 @@
+<?php namespace thrill_seekers\hw3\controllers;
+
+use thrill_seekers\hw3\controllers\Controller;
+use thrill_seekers\hw3\views\ReadStoryView;
+use thrill_seekers\hw3\models;
+
+class ReadStoryController extends Controller
+{
+	public $data;
+	public $view;
+	public function invoke($arg)
+	{
+		$storyid=$arg[0];
+		$this->model->getStory($this,$storyid);
+		$this->model->closeConnection();
+		//check if story is rated and if so set appropriate values
+		if(isset($_SESSION['ratedstories'][$storyid]))
+		{
+			$this->data['showuserrating']=$_SESSION['ratedstories'][$storyid];
+		}
+		
+		$formatteddata=\preg_replace("/&#13;&#10;/","&#10;",$this->data['content']);
+		$this->data['paragraphchunks']=\explode("&#10;&#10;",$formatteddata);
+		$this->view=new ReadStoryView();
+		$this->view->render($this->data);
+	}
+	public function invokeRateStory($arg)
+	{
+		if(count($arg)!=2)
+		{
+			print("Error!Cannot rate. Insufficient arguments passed");
+		}
+		else
+		{
+			$storyid=$arg[0];
+			$rating=$arg[1];
+			$_SESSION['ratedstories'][$storyid]=$rating;
+			$this->model->rateStory($this,$storyid,$rating);
+			$this->model->closeConnection();
+			$this->data['showuserrating']=$_SESSION['ratedstories'][$storyid];
+			$formatteddata=\preg_replace("/&#13;&#10;/","&#10;",$this->data['content']);
+			$this->data['paragraphchunks']=\explode("&#10;&#10;",$formatteddata);
+			$this->view=new ReadStoryView();
+			$this->view->render($this->data);
+		}
+	}
+}
