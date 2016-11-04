ad5ab413dcd6d958033939f72105bb920953eaf4
vasuvenk <vasu18@gmail.com>
2016-11-03 23:35:45 -0700

    Homework 4 setups

--- Hw4/index.php
+++ Hw4/index.php
@@ -4,8 +4,8 @@ namespace thrill_seekers\hw4;
 use thrill_seekers\hw4\controllers as C;
 use thrill_seekers\hw4\configs\Config;
 
-if(!isset($_REQUEST['c']) && !isset($_REQUEST['m']))
+if(!isset($_REQUEST['c']))
 {
 	$controller=new C\LandingController();
 	$controller->invoke();
-}
+} ?>
